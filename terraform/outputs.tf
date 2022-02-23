1d402782a3637a337c4f26815bca26a368056574
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-02-24 00:35:28 +0500

    убрал конфиг тераформ

--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "public_ip" {
-  value = toset([
-    for instance in yandex_compute_instance.vm : "${instance.name} ${instance.network_interface.0.nat_ip_address}"
-  ])
-  description = "Публичный IP инстансов в Y.Cloud"
-}
